f4e6d81900afef384b8a13b44c89941c8b3634d3
hdconfigure <hd.configure.direct@gmail.com>
2026-08-19 00:21:43 -0700

    Update generate_page.bash

--- generate_page.bash
+++ generate_page.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
+#copyright (c) 2026 Theron Kauk and John Morris Beck
 generate_page(){
 
-path="$1"
-cat "$1/generate_page.js" "$2" > "/tmp/generate_page$$";
-node "/tmp/generate_page$$" > "$3"
+    path="$1"
+    pagefile="$2"
+    outputpagepath="$3"
+cat "$path/generate_page.js" "$pagefile" > "/tmp/generate_page$$";
+node "/tmp/generate_page$$" > "$outputpagepath"
 rm "/tmp/generate_page$$";
 
 };
-generate_page;
+generate_page "$@";
