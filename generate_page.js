735c0274a39e05901376c25e00a1ab475bb19c34
hdconfigure <hd.configure.direct@gmail.com>
2026-08-18 23:09:52 -0700

    Update generate_page.js
    fixed broken style link

--- generate_page.js
+++ generate_page.js
@@ -5,7 +5,7 @@ var new_page=(title,)=>{
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Your Page Title</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 
