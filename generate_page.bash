55bfebb05a91f6f0a62c2df1804ef3549519ae87
hdconfigure <hd.configure.direct@gmail.com>
2026-08-19 15:44:38 -0700

    Update generate_page.bash
    added markdown_like_page_language notation. havent debugged it yet

--- generate_page.bash
+++ generate_page.bash
@@ -1,10 +1,55 @@
-#!/bin/bash
+#!/bin/sh
+#copyright 2026 Theron Kauk and John Morris Beck - hd.configure.direct@gmail.com - all rights reserved
+markdown_like_page_language(){ awk '
+
+#todo
+#sanitize inputs into markdown-like language
+#risklevel severe
+
+#detect if we are interpreting markdown_like_page_language or javascript
+$0=="markdown_like_page_language"{
+printf "%s" "console.log("$1"(";
+interpet_markdown_like_page_language=1}
+
+#if we are not interpreting markdown_like_page_language print $0
+!interpret_markdown_like_page_language;
+
+#if we are interpreting markdown_like_page_language
+interpreting_markdown_like_page_language{
+
+
+#determine if we are finished interpreting markdown_like_page_language
+while(interpreting_markdown_like_page_language){
+getline line_of_markdown_language;
+if(line_of_markdown_language=="markdown_like_page_language"){
+interpreting_markdown_like_page_language=0;
+printf "%s" "));"
+}
+
+#if we are not inished interpreting markdown_like_langauge
+else{
+
+printf "%s" "\"" $0 "\"" ",";
+
+}
+
+}
+
+}
+
+
+';};
+
+
 generate_page(){
+
+
+    #create the javascript file that generates the new page
     pagefile="$1"
-    #todo: change this to be dynamic path
-cat "/home/user/projects/flightdeckexperiences/generate_page.js" "$pagefile" > "/tmp/generate_page$$";
-node "/tmp/generate_page$$"
-rm "/tmp/generate_page$$";
+    cat "/home/user/projects/flightdeckexperiences/generate_page.js" "$pagefile" | markdown_like_page_language #> "/tmp/generate_page$$" 
+
+#    node "/tmp/generate_page$$"
+#    rm "/tmp/generate_page$$";
 
 };
 generate_page "$@";
