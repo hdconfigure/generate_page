6c2b32da69a9d5417aaf7b58ea963d76a6fcb3ad
hdconfigure <hd.configure.direct@gmail.com>
2026-08-19 19:50:19 -0700

    Update generate_page.js

--- generate_page.js
+++ generate_page.js
@@ -25,10 +25,9 @@ var new_page=(license,title,style,body)=>{
 //make a navbar for the page
 var top_bar=(link)=>{return `
   <div class="top-bar">
-      <span class="menu-text">Menu</span>
     <a href="${link}" class="menu-link" title="Open Menu">
-
       <img src="/assets/menu.png" alt="Menu">
+      <span class="menu-text">Menu</span>
     </a>
   </div>`;
                     }
